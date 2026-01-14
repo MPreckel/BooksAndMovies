2ce40cd855a5074b2af07fa9d73926962373fa88
unknown <manuel.preckel@minimalart.co>
2026-01-14 14:25:43 -0300

    add some padding

--- src/app/books/[slug]/page.tsx
+++ src/app/books/[slug]/page.tsx
@@ -68,7 +68,7 @@ export default async function BookDetailPage({ searchParams }: BookDetailPagePro
                     volumeInfo.imageLinks?.thumbnail;
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 py-8">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 py-16">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex flex-col md:flex-row gap-8">
           {/* Cover */}
